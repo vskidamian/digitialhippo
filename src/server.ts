0b77d3bc5dc626907031baf9ebb5cc4bdcd545ba
vskidamian <jakubowski1337@gmail.com>
2023-12-07 17:53:00 +0100

    final server config + sign up component

--- src/server.ts
+++ src/server.ts
@@ -18,9 +18,8 @@ const start = async () => {
     app.use((req, res) => nextHandler(req, res));
 
     nextApp.prepare().then(() => {
-        payload.logger.info('Next.js started');
 
-        app.listen(PORT, async () => payload.logger.info(`Next.js App url: ${process.env.NEXT_PUBLIC_SERVER_URL}`))
+        app.listen(PORT, async () => {})
     })
 }
 
