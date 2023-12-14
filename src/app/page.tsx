68856dfb7aa1f770349ee592b8401ad70f546b61
vskidamian <jakubowski1337@gmail.com>
2023-12-14 13:57:40 +0100

    Working on Product Reel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -27,9 +28,13 @@ export default function Home() {
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Your marketplace for high-qualtiy <span className="text-blue-600">digital assets</span>.
+            Your marketplace for high-qualtiy{" "}
+            <span className="text-blue-600">digital assets</span>.
           </h1>
-          <p className="mt-6 text-lg max-w-prse text-muted-foreground">Welcome to DigitalHippo. Every asset on the platform is verified by our team to ensure our highest quality standards.</p>
+          <p className="mt-6 text-lg max-w-prse text-muted-foreground">
+            Welcome to DigitalHippo. Every asset on the platform is verified by
+            our team to ensure our highest quality standards.
+          </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
               Browse Trending
@@ -38,20 +43,28 @@ export default function Home() {
           </div>
         </div>
 
-        {/*TODO: List products*/}
+        <ProductReel title="Brand new" href="/products" />
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
           <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
             {perks.map((perk) => (
-              <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
                 <div className="md:flex-shrink-0 flex justify-center">
-                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-500">{<perk.Icon className="w-1/3 h-1/3 " />}</div>
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-500">
+                    {<perk.Icon className="w-1/3 h-1/3 " />}
+                  </div>
                 </div>
                 <div className="mt-6 md:ml04 md:mt-0 lg:ml-0 lg:mt-6">
-                  <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
-                  <p className="mt-3 text-sm text-muted-foreground">{perk.desc}</p>
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.desc}
+                  </p>
                 </div>
               </div>
             ))}
@@ -61,3 +74,4 @@ export default function Home() {
     </>
   );
 }
+
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,22 @@
+import { z } from "zod";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "@/lib/validators/query-validator";
 
 export const appRouter = router({
-    auth: authRouter
-})
+  auth: authRouter,
+  getInfiniteProducts: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(),
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input;
+      const { sort, limit, ...queryOptions } = query;
+    }),
+});
 
 export type AppRouter = typeof appRouter;
