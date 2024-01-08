557c0fa32399165389c2cbda8954645d3d90db4d
vskidamian <jakubowski1337@gmail.com>
2024-01-08 21:39:40 +0100

    privateProcedure

---
+++ src/trpc/payment-router.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+import { privateProcedure, publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { getPayloadClient } from "../get-payload";
+
+export const paymentRouter = router({});
